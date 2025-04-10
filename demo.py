1dceeddf6fc340e826b32bc492600d6e6df0d13b
Swathi <khanadaliswatis@gmail.com>
2025-04-10 07:01:36 +0000

    Saved local changes to demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
-#!/bin/bash
-echo <<Python is a high-level, easy-to-read, powerful programming language that
-Loved by beginners for its simplicity 
-Respected by pros for its versatility 
-Used everywhere — from building websites to training AI to automating your life>>
+#!/usr/bin/env python3
+
+print("""Python is a high-level, easy-to-read, powerful programming language that
+Loved by beginners for its simplicity
+Respected by pros for its versatility
+Used everywhere from building websites to training AI to automating your life.""")
