ebc361b21056c308442d540d566083c87bf469f7
Swathi <khanadaliswatis@gmail.com>
2025-04-10 07:08:37 +0000

    created new shell scripting file

---
+++ loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hey there! 👋 What's your name?"
+read name
+
+echo "Nice to meet you, $name! 🌟"
+echo "The current time is: $(date +"%r")"
+echo "Hope you're having a great day! 😊"
+
