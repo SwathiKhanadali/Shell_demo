eb597ab02901a3a09edc5fee1c583bd3923c7dc4
SwathiKhanadali <91979017+SwathiKhanadali@users.noreply.github.com>
2025-04-10 11:49:11 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo <<Python is a high-level, easy-to-read, powerful programming language that’s:
+Loved by beginners for its simplicity ✨
+Respected by pros for its versatility 🔧
+Used everywhere — from building websites to training AI to automating your life>>
