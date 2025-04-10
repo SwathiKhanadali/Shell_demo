e049aa3847479d2ecd94198420707f4d5b22addb
SwathiKhanadali <91979017+SwathiKhanadali@users.noreply.github.com>
2025-04-10 11:59:57 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo <<Python is a high-level, easy-to-read, powerful programming language that’s:
-Loved by beginners for its simplicity ✨
-Respected by pros for its versatility 🔧
+Loved by beginners for its simplicity 
+Respected by pros for its versatility 
 Used everywhere — from building websites to training AI to automating your life>>
