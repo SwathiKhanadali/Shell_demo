3d610850b4e589fdd1680989933a352ca0f81958
SwathiKhanadali <91979017+SwathiKhanadali@users.noreply.github.com>
2025-04-10 12:11:03 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo <<Python is a high-level, easy-to-read, powerful programming language that’s:
+echo <<Python is a high-level, easy-to-read, powerful programming language that
 Loved by beginners for its simplicity 
 Respected by pros for its versatility 
 Used everywhere — from building websites to training AI to automating your life>>
